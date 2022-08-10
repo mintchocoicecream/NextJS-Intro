df8bcee2fb7edea5d37a4378b44a6a6bbd52d92b
mintchocoicecream <sxxhnjng@gmail.com>
2022-08-10 11:40:32 +0900

    #2-1. Patterns
    Add Layout.js & Seo.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,10 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({Component, pageProps}){
     return (
-        <div>
-            <NavBar />
+        <Layout>
             <Component {...pageProps} />
-            <span>hello</span>
-            <style jsx global>{
-                `
-                a {
-                    color: yellow;
-                }
-                `
-            }
-            </style>
-        </div>
+        </Layout>
     );
 }
